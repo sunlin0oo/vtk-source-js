8a30c2f72dc4107e5d220ad4adfad879d7edde85
sunlinMain <sunzifeng@simverse.cn>
2022-11-10 14:41:00 +0800

    修改直接读取VTP文件

--- Sources/Filters/General/ClipClosedSurface/example/index.js
+++ Sources/Filters/General/ClipClosedSurface/example/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable no-unused-vars */
 import 'vtk.js/Sources/favicon';
 
@@ -14,8 +15,10 @@ import vtkSphereSource from 'vtk.js/Sources/Filters/Sources/SphereSource';
 import vtkXMLPolyDataReader from 'vtk.js/Sources/IO/XML/XMLPolyDataReader';
 import vtkPolyData from 'vtk.js/Sources/Common/DataModel/PolyData';
 import vtkDataArray from 'vtk.js/Sources/Common/Core/DataArray';
+import HttpDataAccessHelper from 'vtk.js/Sources/IO/Core/DataAccessHelper/HttpDataAccessHelper';
 import displacementData from '../../../../../Data/JSON/displacement.json';
 
+const fileName = 'beam.vtp';
 // ----------------------------------------------------------------------------
 // Example code
 // ----------------------------------------------------------------------------
@@ -25,11 +28,11 @@ console.log('displacementData', displacementData);
 const actor = vtkActor.newInstance();
 const mapper = vtkMapper.newInstance({ interpolateScalarBeforeMapping: true });
 actor.setMapper(mapper);
-// const NAMED_COLORS = {
-//   BANANA: [110 / 255, 207 / 255, 87 / 255], // color2
-//   TOMATO: [255 / 255, 99 / 255, 71 / 255], // color1
-//   SANDY_BROWN: [244 / 255, 164 / 255, 96 / 255], // color3
-// };
+const NAMED_COLORS = {
+  BANANA: [255 / 255, 255 / 255, 255 / 255], // color2==>clipColor
+  TOMATO: [255 / 255, 0 / 255, 0 / 255], // color1==>baseColor
+  SANDY_BROWN: [244 / 255, 164 / 255, 96 / 255], // color3
+};
 
 // ----------------------------------------------------------------------------
 
@@ -48,97 +51,113 @@ function update() {
   cam.setClippingRange(0.1, 50.0);
 
   // Build pipeline
-  const source = reader.getOutputData(0);
-  console.log('source', source.getCellData());
-  const sourcePoly = reader.getOutputData(2);
-  // --------------------- 设置output ---------------------
-  console.log('sourcePoly', sourcePoly.toJSON());
-  const inputLines = source.getLines();
-  sourcePoly.getPolys().setData(inputLines.getData());
-  // const offsetVBO = new Float32Array(inputLines.getData().length);
-  // let offsetVBOIndex = 0;
-  // eslint-disable-next-line array-callback-return
-  // Object.values(displacementData).map((value) => {
-  //   offsetVBOIndex++;
-  //   offsetVBO[offsetVBOIndex] = value * 100;
-  // });
-  // sourcePoly.getCellData().setScalars(
-  //   vtkDataArray.newInstance({
-  //     name: 'Normals',
-  //     values: offsetVBO,
-  //     numberOfComponents: 3,
+  // --------------------- 设置球体 ---------------------
+  // const source = vtkSphereSource
+  //   .newInstance({
+  //     thetaResolution: 20,
+  //     phiResolution: 11,
   //   })
-  // );
+  //   .getOutputData();
   // --------------------- END ---------------------
+  const sourcePoly = reader.getOutputData(0);
+  // --------------------- 设置output 仅有外边框的 ---------------------
+  const source = reader.getOutputData(2);
+  console.log('sourcePoly', source.toJSON());
+  const inputLines = sourcePoly.getLines();
+  source.getPolys().setData(inputLines.getData());
+  // --------------------- END ---------------------
+  // 获取边缘
+  // const bounds = source.getOutputData().getBounds();
   const bounds = source.getBounds();
+  // 获得中心点
   const center = [
     (bounds[1] + bounds[0]) / 2,
     (bounds[3] + bounds[2]) / 2,
     (bounds[5] + bounds[4]) / 2,
   ];
+
   const planes = [];
-  const plane1 = vtkPlane.newInstance({
-    origin: center,
-    normal: [1.0, 0.0, 0.0],
-  });
-  planes.push(plane1);
+  // const plane1 = vtkPlane.newInstance({
+  //   origin: center,
+  //   normal: [1.0, 0.0, 0.0], // x轴
+  // });
+  // planes.push(plane1);
+
   const plane2 = vtkPlane.newInstance({
     origin: center,
-    normal: [0.0, 1.0, 0.0],
+    normal: [0.0, 1.0, 0.0], // y轴
   });
-  // planes.push(plane2);
-  // const plane3 = vtkPlane.newInstance({
-  //   origin: center,
-  //   normal: [0.0, 0.0, 1.0],
-  // });
-  // planes.push(plane3);
+  planes.push(plane2);
 
-  const filter = vtkClipClosedSurface.newInstance({
-    clippingPlanes: planes,
-    activePlaneId: 2,
-    passPointData: true,
+  const plane3 = vtkPlane.newInstance({
+    origin: center,
+    normal: [0.0, 0.0, 1.0], // z轴
   });
-  filter.setInputData(sourcePoly);
+  planes.push(plane3);
+  console.time('Filter');
+  const filter = vtkClipClosedSurface.newInstance({
+    clippingPlanes: planes, // 设定一组用来裁剪的隐函数平面集合
+    activePlaneId: 2, // 如果设置了ActivePlaneId，则为通过使用ActivePlane剪裁生成的任何新几何体设置颜色。 设定当前活动平面ID为2==>对应数组的索引
+    passPointData: true, // 可以获取到顶点的标量==>Pass data from one fieldData to another at the given index==>passData方法于DataSetAttributes类中
+    // clipColor: NAMED_COLORS.BANANA,
+    // baseColor: NAMED_COLORS.TOMATO,
+    // activePlaneColor: NAMED_COLORS.SANDY_BROWN,
+    // GenerateOutline: true, // 控制是否在输入面被平面切割的位置生成轮廓
+    // generateFaces: true, // 设置是否为输出生成多边形面此选项处于启用状态。如果关闭，则输出将没有poly。
+    // triangulatePolys: true,
+    // tolerance: true,
+  });
+
+  // filter.setInputConnection(source);
+  filter.setInputData(source);
+  // filter.setScalarModeToColors();
   filter.setScalarModeToNone();
   filter.update();
   const filterData = filter.getOutputData();
+  console.timeEnd('Filter');
   console.log('filterData', filterData.toJSON());
-  mapper.setInputData(sourcePoly);
+  mapper.setInputData(filterData);
   actor.getProperty().setRepresentation(2);
-  // -----------------------------------------------------------
 
+  // -----------------------------------------------------------
+  console.time('render');
   renderer.resetCamera();
   renderWindow.render();
-
   renderer.addActor(actor);
   resetCamera();
   render();
+  console.timeEnd('render');
 }
 
 // ----------------------------------------------------------------------------
 // Use a file reader to load a local file
 // ----------------------------------------------------------------------------
+HttpDataAccessHelper.fetchBinary(
+  `${__BASE_PATH__}/data/model/${fileName}`
+).then((binary) => {
+  reader.parseAsArrayBuffer(binary, true);
+  update();
+});
+// const myContainer = document.querySelector('body');
+// const fileContainer = document.createElement('div');
+// fileContainer.innerHTML = '<input type="file" class="file"/>';
+// myContainer.appendChild(fileContainer);
 
-const myContainer = document.querySelector('body');
-const fileContainer = document.createElement('div');
-fileContainer.innerHTML = '<input type="file" class="file"/>';
-myContainer.appendChild(fileContainer);
+// const fileInput = fileContainer.querySelector('input');
 
-const fileInput = fileContainer.querySelector('input');
+// function handleFile(event) {
+//   event.preventDefault();
+//   const dataTransfer = event.dataTransfer;
+//   const files = event.target.files || dataTransfer.files;
+//   if (files.length === 1) {
+//     myContainer.removeChild(fileContainer);
+//     const fileReader = new FileReader();
+//     fileReader.onload = function onLoad(e) {
+//       reader.parseAsArrayBuffer(fileReader.result, true);
+//       update();
+//     };
+//     fileReader.readAsArrayBuffer(files[0]);
+//   }
+// }
 
-function handleFile(event) {
-  event.preventDefault();
-  const dataTransfer = event.dataTransfer;
-  const files = event.target.files || dataTransfer.files;
-  if (files.length === 1) {
-    myContainer.removeChild(fileContainer);
-    const fileReader = new FileReader();
-    fileReader.onload = function onLoad(e) {
-      reader.parseAsArrayBuffer(fileReader.result, true);
-      update();
-    };
-    fileReader.readAsArrayBuffer(files[0]);
-  }
-}
-
-fileInput.addEventListener('change', handleFile);
+// fileInput.addEventListener('change', handleFile);
